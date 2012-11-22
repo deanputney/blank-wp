2fbcc8e749c2382e03f70b68b822b5010c290548
Dean Putney <mustardhamsters@gmail.com>
2012-11-21 20:35:12 -0800

    cachebusting

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
   <title></title>
-  <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=0" />
+  <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=1" />
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"> </script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js?v=0"> </script>
 </head>
